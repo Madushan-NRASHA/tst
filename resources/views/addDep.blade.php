b3b2ae31b017adf6fb7862d1c60acc7982ee36d8
gaddganepola <ganeypola@gmail.com>
2025-03-26 10:42:06 +0530

    Department section updates done.

--- resources/views/addDep.blade.php
+++ resources/views/addDep.blade.php
@@ -23,7 +23,7 @@
         <section class="content">
             <div class="card card-info">
                 <div class="card-header">
-                    <h3 class="card-title">Departments</h3>
+                    <h3 class="card-title">Add New Department</h3>
                 </div>
 
                 <form class="form-horizontal" method="POST" action="{{ route('admin.storeDep') }}">
@@ -32,14 +32,14 @@
                         <div class="form-group row">
                             <label for="categoryName" class="col-sm-2 col-form-label">Department Name</label>
                             <div class="col-sm-10">
-                                <input type="text" class="form-control" id="categoryName" name="category_name" placeholder="Enter category name" required>
+                                <input type="text" class="form-control" id="categoryName" name="category_name" placeholder="Enter Department Name" required>
                             </div>
                         </div>
                     </div>
 
                     <div class="card-footer">
-                        <button type="submit" class="btn btn-info">Save</button>
-                        <button type="button" class="btn btn-default float-right" onclick="window.history.back()">Cancel</button>
+                        <button type="submit" class="btn btn-info">Add Department</button>
+                        <button type="button" class="btn btn-default float-right" onclick="location.reload();">Cancel</button>
                     </div>
                 </form>
             </div>
@@ -53,7 +53,7 @@
 
                         <div class="card">
                             <div class="card-header">
-                                <h3 class="card-title">Main Category Table</h3>
+                                <h3 class="card-title">Department Table</h3>
                             </div>
                             <div class="card-body">
 
@@ -74,9 +74,7 @@
                                                 <a href="{{ route('admin.editCatogary', $dep->id) }}" class="text-warning">
                                                     <span class="btn btn-warning"><i class="fas fa-edit"></i> Edit</span>
                                                 </a>
-                                                <a href="{{ route('admin.destroyCatogary', $dep->id) }}" class="text-danger" onclick="confirmDelete(event, '{{ route('admin.destroyCatogary', $dep->id) }}')
-
-">
+                                                <a href="{{ route('admin.destroyCatogary', $dep->id) }}" class="text-danger" onclick="confirmDelete(event, '{{ route('admin.destroyCatogary', $dep->id) }}')">
                                                     <span class="btn btn-danger"><i class="fas fa-trash"></i> Delete</span>
                                                 </a>
 
@@ -107,7 +105,7 @@
         <script>
             function confirmDelete(event, url) {
                 event.preventDefault();
-                if (confirm('Are you sure you want to delete this category? if You Delete ThiS Company All Users Also Removing AutoMaicly And This action cannot be undone.')) {
+                if (confirm('Are you sure you want to delete this department? if You Delete This All Users Within This Department Will Removing AutoMaicly And This action cannot be undone.')) {
                     window.location.href = url;
                 }
             }
